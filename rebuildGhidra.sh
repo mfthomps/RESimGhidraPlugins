721ac2219eb05b224a30a332c8f14b5c482d0d64
Mike Thompson <mfthomps@nps.edu>
2022-11-01 12:00:47 -0700

    Fix path to install directory, and create it if needed.

--- rebuildGhidra.sh
+++ rebuildGhidra.sh
@@ -12,6 +12,9 @@ cd $GHIDRA_DEV_DIR
 git pull
 gradle -I gradle/support/fetchDependencies.gradle init
 gradle buildGhidra
+mkdir -p $GHIDRA_INSTALL_DIR
+# Assumes GHIDRA_INSTALL_DIR is x/y/Ghidra
 cd $GHIDRA_INSTALL_DIR
+cd ../../
 zip=$(ls -Art $GHIDRA_DEV_DIR/build/dist/*.zip | tail -n 1)
 unzip $zip
