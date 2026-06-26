76a6f2adf9529f0f9eb38f3ffd41abd9f186fc43
mike <mfthomps@nps.edu>
2026-06-26 10:37:41 -0700

    update release procedure

--- release/mkrelease.sh
+++ release/mkrelease.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #
 # Create a RESim Ghidra plugins release.
+# Assumes you've exported the jar file using the GhidraDev plugin
+# and the result is in ~/eclipse-workspace/RESimPlugin/dist
 #
 #
-release_dir=$HOME/resimGhidraRelease/RESimGhidraPlugins
-if [[ ! -d $release_dir ]]; then
-    echo "No $release_dir directory found"
-    exit 
-fi
 if [[ -z "$1" ]]; then
     tag=$(git tag | tail -n 1)
     echo "Missing tag, most recent is "$tag" .  Pick the next revision."
@@ -25,26 +22,20 @@ if [[ ! -d release ]]; then
     echo "Run it from the parent directory, e.g., ./release/mkrelease.sh"
     exit 
 fi
-git pull || exit
-git push --set-upstream origin master || exit
+git checkout master || exit
+dist_dir=~/eclipse-workspace/RESimPlugin/dist
+latest=$(ls -t $dist_dir | head -n 1)
+echo "Will release latest: $latest"
+full=$dist_dir/$latest
+rm -fr artifacts
+mkdir -p artifacts
+cp $full artifacts/
 
 new_tag=$1
 here=`pwd`
 
-mkdir -p $release_dir/lib
-mkdir -p release/artifacts
-here=`pwd`
-cp lib/*.jar $release_dir/lib
-cp build/libs/RESimGhidraPlugins.jar $release_dir/lib
-cp extension.properties $release_dir/
-
-cd $release_dir
-cd ..
-tar czf $here/release/artifacts/RESimGhidraPlugins.tar RESimGhidraPlugins
-echo "Now generate release"
-cd $here
 git tag $new_tag
-git push --set-upstream origin master
+#git push --set-upstream origin master
 git push --tags
 
 github-release release --security-token $gitpat --user mfthomps --repo RESimGhidraPlugins --tag $new_tag
@@ -54,5 +45,5 @@ while [ -z "$(github-release info --security-token $gitpat --user mfthomps --rep
     sleep 2
 done
 echo "Upload RESim plugins"
-github-release upload --security-token $gitpat --user mfthomps --repo RESimGhidraPlugins --tag $new_tag --name RESimGhidraPlugins.tar --file release/artifacts/RESimGhidraPlugins.tar
+github-release upload --security-token $gitpat --user mfthomps --repo RESimGhidraPlugins --tag $new_tag --name $latest --file release/artifacts/$latest
 #git fetch --tags
